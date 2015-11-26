5766bd4c4e6e800aff33b662e8ae60585de3ef09
roylines <roy@roylines.co.uk>
2015-11-26 12:57:45 +0000

    added failing test

--- test/index.js
+++ test/index.js
@@ -1,8 +1,15 @@
 const Lab = require("lab");
 const lab = exports.lab = Lab.script();
+const should = require("chai").should;
+
+should();
 
 lab.experiment("plight", () => {
   lab.test("should", (done) => {
+    return done();
+  });
+  lab.test("should fail", (done) => {
+    true.should.equal(false);
     return done();
   });
 });
